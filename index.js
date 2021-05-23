d04ca5a369eda39df956369f10753acddac6d7cf
mdyousuf912 <mdyousuf91298@gmail.com>
2021-05-23 10:40:06 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+var a = 3;
+console.log(a)
+
+var b=2, c=3;
+if( b> c){
+    console.log(b)
+}
+else{
+    console.log(c)
+}
+
+
+// function add(d, e){
+//     var sum= d+e;
+//     console.log('sum is ' +sum)
+// }
+
+// add(9,9);
